997dd8cc7064126d0fced8fbb4b67fb41e612826
Boykinmr <36653149+Boykinmr@users.noreply.github.com>
2018-12-01 20:58:00 -0700

    Updated Settings with new view and changed fragment to list the dates.

--- app/src/main/java/com/msu/msuconnect/Settings.kt
+++ app/src/main/java/com/msu/msuconnect/Settings.kt
@@ -5,24 +5,45 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+import java.util.*
 
 
 class Settings : Fragment() {
 
     companion object {
-        fun newInstance() : Settings {
+        fun newInstance(): Settings {
             return Settings()
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_settings, container, false)
+        var calMonth = Calendar.getInstance().get(Calendar.MONTH) + 1
+        var calDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        var calYear = Calendar.getInstance().get(Calendar.YEAR)
+
         /*If private is selected as a setting turn off public if on
           If public is selected as a setting turn off private if on
           Taken care of because the selections are in radio group*/
+
+        //Set Start Date
+
+        view.calendarView.setOnDateChangeListener { calendarView, i1, i2, i3 ->
+            calYear = i1
+            //calMonth = i2 + 1
+            calDay = i3
+        }
+
+        view.Button_StartDate.setOnClickListener {
+            view.textViewStart.text = "$calMonth/$calDay/$calYear"
+        }
+
+        //Set End Date
+        view.Button_EndDate.setOnClickListener {
+            view.textViewEnd.text = "$calMonth/$calDay/$calYear"
+        }
+        return view
     }
-
-
 
 }
