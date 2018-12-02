dc6996f5282ab54a61cafa526fb17a0a13857077
Boykinmr <36653149+Boykinmr@users.noreply.github.com>
2018-12-01 21:05:52 -0700

    I corrected the date information to store values in long format and in integer formats.  The calYear, calDay etcs are all integar values if you need those too.  The long formats are the rtnStartDate and rtnEndDate.  Feel free to delete the values you don't use.

--- app/src/main/java/com/msu/msuconnect/Settings.kt
+++ app/src/main/java/com/msu/msuconnect/Settings.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_settings.*
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 import java.util.*
 
@@ -31,17 +32,25 @@ class Settings : Fragment() {
 
         view.calendarView.setOnDateChangeListener { calendarView, i1, i2, i3 ->
             calYear = i1
-            //calMonth = i2 + 1
+            calMonth = i2 + 1
             calDay = i3
         }
 
         view.Button_StartDate.setOnClickListener {
             view.textViewStart.text = "$calMonth/$calDay/$calYear"
+            val rtnStartMonth = calMonth
+            val rtnStartDay = calDay
+            val rtnStartYear = calYear
+            val rtnStartDate = calendarView.getDate()
         }
 
         //Set End Date
         view.Button_EndDate.setOnClickListener {
             view.textViewEnd.text = "$calMonth/$calDay/$calYear"
+            val rtnEndMonth = calMonth
+            val rtnEndDay = calDay
+            val rtnEndYear = calYear
+            val rtnEndDate = calendarView.getDate()
         }
         return view
     }
